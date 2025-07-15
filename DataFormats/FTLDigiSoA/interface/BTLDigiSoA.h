69926f5548d12ddb3c6dbf43f4b39516c09c9c2d
cquarant <claudio.quaranta@cern.ch>
2025-07-15 15:52:25 +0200

    redefine SoA to contain 2 channels per entry

--- DataFormats/FTLDigiSoA/interface/BTLDigiSoA.h
+++ DataFormats/FTLDigiSoA/interface/BTLDigiSoA.h
@@ -8,17 +8,27 @@ namespace btldigi {
                         SOA_COLUMN(uint32_t, rawId    ), // Raw ID of the module/TOFHIR
                         SOA_COLUMN(uint16_t, BC0count ),  // BC0 count (reserved)
                         SOA_COLUMN(bool    , status   ),  // status of the TOFHIR
-                        SOA_COLUMN(uint8_t , chID     ), // TOFHIR channel ID
                         SOA_COLUMN(uint32_t, BCcount  ), 
-                        SOA_COLUMN(uint16_t, T1coarse ),
-                        SOA_COLUMN(uint16_t, T2coarse ),  
-                        SOA_COLUMN(uint16_t, EOIcoarse),  
-                        SOA_COLUMN(uint16_t, Charge   ),  
-                        SOA_COLUMN(uint16_t, T1fine   ),  
-                        SOA_COLUMN(uint16_t, T2fine   ),  
-                        SOA_COLUMN(uint16_t, IdleTime ),  
-                        SOA_COLUMN(uint8_t , PrevTrigF),
-                        SOA_COLUMN(uint8_t , TACID    )
+                        SOA_COLUMN(uint8_t , chIDR     ), // TOFHIR channel ID, right side of crystal
+                        SOA_COLUMN(uint16_t, T1coarseR ), // data from crystal right side
+                        SOA_COLUMN(uint16_t, T2coarseR ),  
+                        SOA_COLUMN(uint16_t, EOIcoarseR),  
+                        SOA_COLUMN(uint16_t, ChargeR   ),  
+                        SOA_COLUMN(uint16_t, T1fineR   ),  
+                        SOA_COLUMN(uint16_t, T2fineR   ),  
+                        SOA_COLUMN(uint16_t, IdleTimeR ),  
+                        SOA_COLUMN(uint8_t , PrevTrigFR),
+                        SOA_COLUMN(uint8_t , TACIDR    ),
+                        SOA_COLUMN(uint8_t , chIDL     ), // TOFHIR channel ID, left side of crystal
+                        SOA_COLUMN(uint16_t, T1coarseL ), // data from crystal left side
+                        SOA_COLUMN(uint16_t, T2coarseL ),  
+                        SOA_COLUMN(uint16_t, EOIcoarseL),  
+                        SOA_COLUMN(uint16_t, ChargeL   ),  
+                        SOA_COLUMN(uint16_t, T1fineL   ),  
+                        SOA_COLUMN(uint16_t, T2fineL   ),  
+                        SOA_COLUMN(uint16_t, IdleTimeL ),  
+                        SOA_COLUMN(uint8_t , PrevTrigFL),
+                        SOA_COLUMN(uint8_t , TACIDL    )
     )
 
     using BTLDigiSoA = BTLDigiSoALayout<>;
